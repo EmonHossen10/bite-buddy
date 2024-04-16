c47bafa8feaa4c909ab89d0fdeb730cc6eb879c5
unknown <you@example.com>
2024-04-16 07:58:03 +0600

    added updates

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,6 @@ import Hero from "@/Components/layout/Hero";
 
 export default function Home() {
   return (
-    // <main>
-    //   <h2>This is next app</h2>
-    //   <h3>Now Pull it out!!</h3>
-    //   <h2>This is next app 2</h2>
-
-    //   <Tools></Tools>
-    // </main>
     <>
       <Header></Header>
       <Hero></Hero>
