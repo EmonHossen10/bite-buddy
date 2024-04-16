6a662d35a843c1f35459d65e527679254b389b61
unknown <you@example.com>
2024-04-16 09:55:19 +0600

    added updates to the About Us

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import AboutUs from "@/Components/AboutUs/AboutUs";
 import SectionTitle from "@/Components/Shared/SectionTitle/SectionTitle";
 import Header from "@/Components/layout/Header";
 import Hero from "@/Components/layout/Hero";
@@ -7,37 +8,7 @@ export default function Home() {
     <>
       <Header></Header>
       <Hero></Hero>
-      <section className="shadow-xl my-10 rounded-xl p-3">
-        <SectionTitle
-          subHeading="Mission and Values"
-          heading="About Us"
-        ></SectionTitle>
-        <p className="text-xs text-gray-400 p-5">
-          Whether you are craving comforting classics, exotic cuisines, or
-          healthy options, we have got you covered. Our curated selection of
-          restaurants ensures that every palate finds its perfect match. From
-          sizzling pizzas to gourmet burgers, zesty Thai curries to
-          mouthwatering sushi rolls, there is something to tantalize every taste
-          bud.
-        </p>
-        <p className="text-xs text-gray-400 p-5">
-          Whether you are craving comforting classics, exotic cuisines, or
-          healthy options, we have got you covered. Our curated selection of
-          restaurants ensures that every palate finds its perfect match. From
-          sizzling pizzas to gourmet burgers, zesty Thai curries to
-          mouthwatering sushi rolls, there is something to tantalize every taste
-          bud.
-        </p>
-        <p className="text-xs text-gray-400 p-5">
-          Whether you are craving comforting classics, exotic cuisines, or
-          healthy options, we have got you covered. Our curated selection of
-          restaurants ensures that every palate finds its perfect match. From
-          sizzling pizzas to gourmet burgers, zesty Thai curries to
-          mouthwatering sushi rolls, there is something to tantalize every taste
-          bud.
-        </p>
-
-      </section>
+      <AboutUs/>
     </>
   );
 }
